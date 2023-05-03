2d19523d0949c48dec5b2f123777d9483af9eda9
Jonathan Valencia <javalencis@gmail.com>
2023-05-03 12:20:31 -0500

    Actualizacion visual

--- src/components/SectionInformacion/SectionInformacion.jsx
+++ src/components/SectionInformacion/SectionInformacion.jsx
@@ -1,14 +1,15 @@
 import './SectionInformacion.scss'
-import VideoTest from '../../assets/video.mp4'
+import VideoDesktop from '../../assets/videos/video-desktop.mp4'
 export const SectionInformacion = () => {
   return (
     <section className="SectionInformacion">
+      <video src={VideoDesktop} controls={false} autoPlay loop muted />
       <div className="Informacion">
         <h2>
-          Generating meaningful tests for busy devs
+          Generating <br /> meaningful tests <br /> for busy devs
         </h2>
         <p>
-          With TestopIA, you get non-trivial tests (and trivial, too!) suggested right inside your IDE, so you can code smart, create more value, and stay confident when you push.
+          With TestopIA, you get non-trivial tests (and trivial, too!) <br /> suggested right inside your IDE, so you can code smart,<br /> create more value, and stay confident when you push.<br />
           Code, as you meant it.
         </p>
         <button>
@@ -16,7 +17,7 @@ export const SectionInformacion = () => {
         </button>
       </div>
       <div className="Animacion">
-        
+
       </div>
     </section>
   )
