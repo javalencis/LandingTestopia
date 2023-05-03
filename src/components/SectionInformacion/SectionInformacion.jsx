912685de61968a5cb529d174558a5b1f2e22cd39
Jonathan Valencia <javalencis@gmail.com>
2023-05-03 10:42:30 -0500

    Seccion informacion

--- src/components/SectionInformacion/SectionInformacion.jsx
+++ src/components/SectionInformacion/SectionInformacion.jsx
@@ -1,7 +1,23 @@
+import './SectionInformacion.scss'
+import VideoTest from '../../assets/video.mp4'
 export const SectionInformacion = () => {
   return (
-    <section>
+    <section className="SectionInformacion">
+      <div className="Informacion">
+        <h2>
+          Generating meaningful tests for busy devs
+        </h2>
+        <p>
+          With TestopIA, you get non-trivial tests (and trivial, too!) suggested right inside your IDE, so you can code smart, create more value, and stay confident when you push.
+          Code, as you meant it.
+        </p>
+        <button>
+          Subscribe
+        </button>
+      </div>
+      <div className="Animacion">
         
+      </div>
     </section>
   )
 }
